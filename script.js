378b1965b1d90a371e12dc84613eb036c5fa4d7b
Kevin Solanki <142715013+itskvsin@users.noreply.github.com>
2024-01-18 19:45:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showsidebar(){
+    const sidebar = document.querySelector(".responsive-nav")
+    sidebar.style.display = 'flex'
+}
+function hidesidebar(){
+    const hidesidebar = document.querySelector(".responsive-nav")
+    hidesidebar.style.display = 'none'
+}
