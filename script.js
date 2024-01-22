bb8f86fc4f0c5855eda35573c52de8604767b10b
Kevin Solanki <142715013+itskvsin@users.noreply.github.com>
2024-01-22 20:18:38 +0530

    Modified the Js For responsiveness

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function showsidebar(){
-    const sidebar = document.querySelector(".responsive-nav")
-    sidebar.style.display = 'flex'
-}
-function hidesidebar(){
-    const hidesidebar = document.querySelector(".responsive-nav")
-    hidesidebar.style.display = 'none'
+//for menubar/navbar, making responsive
+let menu_icon = document.getElementById("menu_icon");
+let navlist = document.getElementById("navlist"); 
+menu_icon.onclick = () => {
+    navlist.classList.toggle("linkclose");
+    navlist.classList.toggle("linkopen");
+    if(navlist.className == "linkopen"){
+        menu_icon.innerHTML = "\u00d7";
+    }
+    else{
+        menu_icon.innerHTML = "☰";
+    }
 }
